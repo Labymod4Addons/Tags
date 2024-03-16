8f0ffa3ca46716f066712fc2a81fea1b997b10f1
xConnorTV <connorbreuer@gmail.com>
2024-03-16 15:17:51 +0100

    Added Config Element (Activity, Widget), Translations

---
+++ core/src/main/java/de/tags/core/TagCommand.java
@@ -0,0 +1,127 @@
+package de.tags.core;
+
+import net.labymod.api.client.chat.command.Command;
+import net.labymod.api.client.chat.command.SubCommand;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.component.format.NamedTextColor;
+import net.labymod.api.client.component.format.TextDecoration;
+
+public class TagCommand extends Command {
+
+
+  protected TagCommand() {
+    super("tags");
+    withSubCommand(new AddCommand());
+    withSubCommand(new RemoveCommand());
+    withSubCommand(new EditCommand());
+
+  }
+
+  @Override
+  public boolean execute(String s, String[] strings) {
+
+    if(strings.length == 0) {
+      this.displayMessage("§c/tags <add|remove|edit>");
+      return true;
+    }
+
+    return true;
+  }
+
+
+  private static class AddCommand extends SubCommand {
+    protected AddCommand() {
+      super("add");
+    }
+
+    @Override
+    public boolean execute(String s, String[] strings) {
+
+      if(strings.length < 2) {
+        this.displayMessage("§c/tags add <Username> <Text>");
+        return true;
+      }
+
+      String username = strings[0];
+      String text = strings[1];
+      Tag newTag = Tag.create(text);
+
+      if(TagsAddon.INSTANCE.configuration().getTags().containsKey(username)) {
+        Tag oldTag = TagsAddon.INSTANCE.configuration().getTags().get(username);
+        TagsAddon.INSTANCE.displayMessage(Component.translatable("tags.command.add.already_exists", NamedTextColor.RED).arguments(Component.text(username, NamedTextColor.GOLD), oldTag.getComponent()));
+        return true;
+      }
+
+      TagsAddon.INSTANCE.configuration().getTags().put(username, newTag);
+
+      TagsAddon.INSTANCE.displayMessage(Component.translatable("tags.command.add.success", NamedTextColor.GRAY).arguments(newTag.getComponent(),Component.text(username, NamedTextColor.GOLD).decorate(TextDecoration.BOLD)));
+
+      return true;
+    }
+  }
+
+  private static class RemoveCommand extends SubCommand {
+    protected RemoveCommand() {
+      super("remove");
+    }
+
+    @Override
+    public boolean execute(String s, String[] strings) {
+      if(strings.length == 0) {
+        this.displayMessage("§c/tags remove <Username>");
+        return true;
+      }
+
+      String username = strings[0];
+
+      if(!TagsAddon.INSTANCE.configuration().getTags().containsKey(username)) {
+        TagsAddon.INSTANCE.displayMessage(Component.translatable("tags.command.not_found", NamedTextColor.RED).argument(Component.text(username, NamedTextColor.GOLD)));
+        return true;
+      }
+
+      Tag oldTag = TagsAddon.INSTANCE.configuration().getTags().get(username);
+
+      TagsAddon.INSTANCE.displayMessage(Component.translatable("tags.command.remove.success", NamedTextColor.GRAY).arguments(oldTag.getComponent(),Component.text(username, NamedTextColor.GOLD).decorate(TextDecoration.BOLD)));
+      TagsAddon.INSTANCE.configuration().getTags().remove(username);
+
+      return true;
+    }
+  }
+
+  private static class EditCommand extends SubCommand {
+    protected EditCommand() {
+      super("edit");
+    }
+
+    @Override
+    public boolean execute(String s, String[] strings) {
+      if (strings.length < 2) {
+        this.displayMessage("§c/tags edit <Username> <Text>");
+        return true;
+      }
+
+      String username = strings[0];
+
+      if (!TagsAddon.INSTANCE.configuration().getTags().containsKey(username)) {
+        TagsAddon.INSTANCE.displayMessage(
+            Component.translatable("tags.command.not_found", NamedTextColor.RED)
+                .argument(Component.text(username, NamedTextColor.GOLD)));
+        return true;
+      }
+
+      String text = strings[1];
+      Tag oldTag = TagsAddon.INSTANCE.configuration().getTags().get(username);
+
+      Component oldTagComponent = oldTag.getComponent();
+      oldTag.setText(text);
+      Component newTagComponent = oldTag.getComponent();
+
+      TagsAddon.INSTANCE.displayMessage(
+          Component.translatable("tags.command.edit.success", NamedTextColor.GRAY)
+              .arguments(Component.text(username, NamedTextColor.GOLD).decorate(TextDecoration.BOLD), newTagComponent, oldTagComponent));
+
+      return true;
+    }
+  }
+
+}
--- core/src/main/java/de/tags/core/TagsConfiguration.java
+++ core/src/main/java/de/tags/core/TagsConfiguration.java
@@ -1,9 +1,16 @@
 package de.tags.core;
 
+import de.tags.core.gui.activity.TagActivity;
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.activity.Activity;
+import net.labymod.api.client.gui.screen.widget.widgets.activity.settings.ActivitySettingWidget.ActivitySetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.Exclude;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import net.labymod.api.util.MethodOrder;
+import java.util.HashMap;
+import java.util.Map;
 
 @ConfigName("settings")
 public class TagsConfiguration extends AddonConfig {
@@ -15,4 +22,24 @@ public class TagsConfiguration extends AddonConfig {
   public ConfigProperty<Boolean> enabled() {
     return this.enabled;
   }
+
+  @Exclude
+  public final Map<String, Tag> tags = new HashMap<>();
+
+  public Map<String, Tag> getTags() {
+    return tags;
+  }
+
+  @MethodOrder(after = "enabled")
+  @ActivitySetting
+  public Activity openNameTags() {
+    return new TagActivity(false);
+  }
+
+
+  public void removeInvalidNameTags() {
+    this.tags.entrySet()
+        .removeIf(entry -> entry.getKey().isEmpty() || entry.getValue().getText().isEmpty());
+  }
+
 }
